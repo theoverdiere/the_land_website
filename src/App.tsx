06baf86df2ba6a1607e379b7e6bc563b8a5f74f7
theoverdiere <theoverdiere@gmail.com>
2023-02-02 00:30:34 +0100

    Add Test Parallax / not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import './App.css';
-import ProjectsPage from './projects/ProjectsPage';
+import backLayer from './imgs/back_layer.jpg';
+import frontLayer from './imgs/front_layer.png';
 
 function App() {
   return (
-    <div className="container">
-      <ProjectsPage/>
+    <div>
+      <Parallax pages={4}>
+
+      <ParallaxLayer offset={0} >
+          <img src ="./imgs/front_layer"  alt="coucou"/>
+      </ParallaxLayer>
+
+      <ParallaxLayer 
+        offset={0}
+        speed={1}
+        factor={4}
+        style ={{
+          backgroundImage: `url(${backLayer})`,
+          backgroundSize: 'cover'
+        }}>
+        </ParallaxLayer>
+      </Parallax>
+
+
+
+
+
+
     </div>
-  );
+  )
+
+
+//   <ParallaxLayer 
+//   offset={0}
+//   style ={{
+//     backgroundImage: `url(${backLayer})`,
+//     backgroundSize: 'cover',
+//   }}>
+// </ParallaxLayer>
+
 }
 
 export default App;
