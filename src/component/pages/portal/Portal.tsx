642cc0c56dca85a0e456f6d9276500213273ca53
theoverdiere <theoverdiere@gmail.com>
2023-02-12 19:19:04 +0100

    Animation

--- src/component/pages/portal/Portal.tsx
+++ src/component/pages/portal/Portal.tsx
@@ -2,15 +2,21 @@ import { motion } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
 import PortalSvg from './svgs/PortalSvgComponent';
 
-
+const svgVariants= {
+    hidden: {rotate: -180},
+    visible: {
+        rotate: 0,
+        transition: {duration : 1}
+    }
+}
 
 function Portal () {
 
     return (
         <div >
             <NavLink to="/the-tales">
-                <motion.svg 
-                                    id="Portail" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 942.72 1501.89">
+                <motion.svg variants={svgVariants} initial="hidden" animate="visible"
+                        id="Portail" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 942.72 1501.89">
                     <PortalSvg></PortalSvg>
                 </motion.svg>
             </NavLink>
